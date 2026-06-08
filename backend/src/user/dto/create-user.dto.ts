f117b3a51108b87a2d9156e5e5f53b7d5a19ceb6
Selli <aabulath@yandex.ru>
2026-06-08 16:29:34 +0400

    add ApiProperty in create-user.dto.ts

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 import { Role as PrismaRole } from '@prisma/client';
 
